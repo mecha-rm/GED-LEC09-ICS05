d1c9a740ec17c62c65fa70173c41b12f16882a91
Roderick Montague <roderick.montague243@gmail.com>
2020-11-13 19:28:15 -0500

    Improved Mobility
    The mobility has been improved so that it's more manageable.  The camera still needs to be changed.

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -9,7 +9,7 @@ public class PlayerBehaviour : MonoBehaviour
     public float verticalForce;
     public float horizontalForce;
 
-    private Vector3 direction;
+    private Vector2 direction;
 
     private Rigidbody2D m_rigidBody2D;
     private SpriteRenderer m_spriteRenderer;
@@ -31,21 +31,45 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(isGrounded)
+
+        // if no direction keys are being pressed.
+        if(direction != Vector2.zero && (!Keyboard.current.aKey.isPressed && !Keyboard.current.leftArrowKey.isPressed
+            && !Keyboard.current.dKey.isPressed && !Keyboard.current.rightArrowKey.isPressed))
         {
-            m_rigidBody2D.AddForce(direction * horizontalForce * Time.deltaTime);
+            direction = Vector2.zero;
+        }
+        
+        // if the direction is not set to 0.
+        if(direction != Vector2.zero)
+        {
+            m_rigidBody2D.AddForce(direction * horizontalForce * Time.deltaTime, ForceMode2D.Force);
+        }
+        // else // this is noot needed since the direction vector is set to Vector2.zero.
+        // {
+        //     // m_rigidBody2D.velocity = new Vector2(m_rigidBody2D.velocity.x * 0.05F, m_rigidBody2D.velocity.y);
+        //     // 
+        //     // if(Mathf.Abs(m_rigidBody2D.velocity.x) < 0.05F)
+        //     // {
+        //     //     m_rigidBody2D.velocity = new Vector2(0.0F, m_rigidBody2D.velocity.y);
+        //     // }
+        // }
+
+        // standing position if not moving
+        if (Mathf.Approximately(m_rigidBody2D.velocity.x, 0.0F))
+        {
+            m_animator.SetInteger("AnimState", 0);
+        }
+
+
+        // Jump
+        if (isGrounded)
+        {
+            // m_rigidBody2D.AddForce(direction * horizontalForce * Time.deltaTime);
 
             if (Mathf.Abs(m_rigidBody2D.velocity.x) > 0.1F)
             {
                 m_animator.SetInteger("AnimState", 1);
             }
-
-            m_rigidBody2D.velocity *= 0.95F;
-        }
-
-        if (Mathf.Approximately(m_rigidBody2D.velocity.x, 0.0F))
-        {
-            m_animator.SetInteger("AnimState", 0);
         }
     }
 
@@ -73,10 +97,6 @@ public class PlayerBehaviour : MonoBehaviour
                 m_spriteRenderer.flipX = false;
                 // m_rigidBody2D.AddForce(Vector2.right * horizontalForce);
                 direction = Vector2.right;
-                break;
-
-            default:
-                direction = Vector2.zero;
                 break;
         }
 
